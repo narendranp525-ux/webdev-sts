e234e443ce11227f46d5448aa9be9d29ebca8956
NARENDRA PARMAR <narendranp525@gmail.com>
2025-12-17 21:15:41 +0530

    Add files via upload

--- first.js
+++ first.js
@@ -1,82 +1,94 @@
-const  score = {
+let score = JSON.parse(localStorage.getItem('massege')) || {
     wins: 0,
     losses: 0,
     tie: 0
-  }
+};
+
+updatescore();
+
+function resetscore() {
+    score.wins = 0,
+    score.losses = 0,
+    score.tie = 0
+    localStorage.removeItem(score);
+    updatescore();
+};
 
 
-  function playgame(playermove) {
+function playgame(playermove) {
     const computerMove = PiccomuterMove();
     let result = '';
 
     if (playermove === 'stone') {
-      if (computerMove === 'stone') {
-        result = 'tie';
-      }
-      else if (computerMove === 'paper') {
-        result = 'lose';
-      }
-      else if (computerMove === 'scissors') {
-        result = 'win';
-      }
+        if (computerMove === 'stone') {
+            result = 'tie';
+        }
+        else if (computerMove === 'paper') {
+            result = 'lose';
+        }
+        else if (computerMove === 'scissors') {
+            result = 'win';
+        }
     }
 
     else if (playermove === 'paper') {
-      if (computerMove === 'stone') {
-        result = 'win';
-      }
-      else if (computerMove === 'paper') {
-        result = 'tie';
-      }
-      else if (computerMove === 'scissors') {
-        result = 'lose';
-      }
+        if (computerMove === 'stone') {
+            result = 'win';
+        }
+        else if (computerMove === 'paper') {
+            result = 'tie';
+        }
+        else if (computerMove === 'scissors') {
+            result = 'lose';
+        }
     }
     else if (playermove === 'scissors') {
-      if (computerMove === 'stone') {
-        result = 'lose';
-      }
-      else if (computerMove === 'paper') {
-        result = 'win';
-      }
-      else if (computerMove === 'scissors') {
-        result = 'tie';
-      }
-    } 
-   
+        if (computerMove === 'stone') {
+            result = 'lose';
+        }
+        else if (computerMove === 'paper') {
+            result = 'win';
+        }
+        else if (computerMove === 'scissors') {
+            result = 'tie';
+        }
+    }
+
     if (result === 'win') {
-    score.wins += 1;
-  }
-  else if (result === 'lose') {
-    score.losses += 1;
-  }
-  else if (result === 'tie') {
-    score.tie += 1;
-  }
-  const strscore = JSON.stringify(score);
-  localStorage.getItem('massege', 'strscore');
+        score.wins += 1;
+    }
+    else if (result === 'lose') {
+        score.losses += 1;
+    }
+    else if (result === 'tie') {
+        score.tie += 1;
+    }
+
+    localStorage.setItem('massege', JSON.stringify(score));
+    updatescore();
+    
+    document.querySelector('.move-js').innerHTML = ` you ${playermove} computer ${computerMove} `;
+    document.querySelector('.result-js').innerHTML = result ;
    
+}
 
-   alert(`you chose ${playermove},computer picked ${computerMove} you ${result}
-wins : ${score.wins} losses :${score.losses} ties : ${score.tie}`
-    );
-  }
- 
- 
-  
+function updatescore() {
+    document.querySelector('.displayscore-js').innerHTML = `wins = ${score.wins} losses = ${score.losses}  ties = ${score.tie}`
+};
 
 
-  let computerMove = '';
-  function PiccomuterMove() {
+
+let computerMove = '';
+function PiccomuterMove() {
     const RandomNumber = Math.random();
 
     if (RandomNumber > 0 && RandomNumber < 1 / 3) {
-      return 'stone';
+        return 'stone';
     }
     else if (RandomNumber >= 1 / 3 && RandomNumber < 2 / 3) {
-      return 'paper';
+        return 'paper';
     }
     else if (RandomNumber >= 2 / 3 && RandomNumber <= 1) {
-      return 'scissors';
+        return 'scissors';
     }
-  }
+}
